018f3f350a81f12dcb4a21f36a813303314a8781
Benjamin <ben.smith.gordon@gmail.com>
2018-06-15 14:56:15 +0100

    Added a private method full and empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,6 @@
 # The docking station for bikes in the Boris Bike project.
 require_relative "bike"
+
 class DockingStation
   attr_reader :bike
 
@@ -9,14 +10,25 @@ class DockingStation
   end
 
   def dock(bike)
-    fail "Docking station full" if @bike.count >= 20
+    fail "Docking station full" if full?
     @bike << bike
   end
 
 
   def release_bike
-    fail "No bikes available" if @bike.empty?
+    fail "No bikes available" if empty?
     @bike.pop
   end
+
+
+private
+
+def full?
+@bike.count >= 20
+end
+
+def empty?
+@bike.empty?
+end
 
 end
