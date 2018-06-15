7276a2d9be40806a6e48436e41c24103f244bb31
Benjamin <ben.smith.gordon@gmail.com>
2018-06-15 17:09:37 +0100

    Adding a broken method and test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,17 @@ require "bike.rb"
 
 describe Bike do
   it { should respond_to(:working?) }
+
+
+it "can be reported broken" do
+  subject.report_broken
+  expect(subject).to be_broken
 end
+end
+
+
+
+
+
+
+
